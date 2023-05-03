d4ab08b7484ac0c5cfdcc0c37a59f6b9b6674ad1
Veronika <kotlyarevska@gmail.com>
2023-05-03 20:59:12 +0200

    events added

--- src/src/js/01-gallery.js
+++ src/src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const imagesEl = galleryItems
+  .map(
+    item =>
+      `<li>
+      <a class="gallery__item" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a></li>`
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', imagesEl);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
